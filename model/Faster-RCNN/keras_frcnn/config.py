ca97491b5225ac82cd8331a04d75198bc6c9197d
SophiaYWang <wangyuxi_an@126.com>
2020-11-08 14:34:36 +1100

    update config file

--- model/Faster-RCNN/keras_frcnn/config.py
+++ model/Faster-RCNN/keras_frcnn/config.py
@@ -16,13 +16,13 @@ class Config:
 		self.rot_90 = False
 
 		# anchor box scales
-		self.anchor_box_scales = [128, 256, 512]
+		self.anchor_box_scales = [64, 128, 256]
 
 		# anchor box ratios
 		self.anchor_box_ratios = [[1, 1], [1./math.sqrt(2), 2./math.sqrt(2)], [2./math.sqrt(2), 1./math.sqrt(2)]]
 
 		# size to resize the smallest side of the image
-		self.im_size = 416
+		self.im_size = 900
 
 		# image channel-wise mean to subtract
 		self.img_channel_mean = [103.939, 116.779, 123.68]
